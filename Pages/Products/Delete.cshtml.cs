068bbc899e11219288ecb746048fa74414ab3cbe
DayanaKCG <165987955+DayanaKCG@users.noreply.github.com>
2024-05-05 20:28:08 -0500

    Add files via upload
    Productos, aun no guarda

---
+++ Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
