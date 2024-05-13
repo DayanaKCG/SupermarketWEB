cc1cf8d064c66ec38df5279c4f099907ce73be79
DayanaKCG <165987955+DayanaKCG@users.noreply.github.com>
2024-05-12 21:33:02 -0500

    Add files via upload

--- Pages/PayModes/Index.cshtml.cs
+++ Pages/PayModes/Index.cshtml.cs
@@ -1,28 +1,27 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SupermarketWEB.Data;
 using SupermarketWEB.Models;
 
-namespace SupermarketWEB.Pages.Providers
+namespace SupermarketWEB.Pages.PayModes
 {
-	[Authorize]
 	public class IndexModel : PageModel
-    {
+	{
 		private readonly SupermarketContext _context;
+
 		public IndexModel(SupermarketContext context)
 		{
 			_context = context;
 		}
-		public IList<Provider> Providers { get; set; } = default!;
+		public IList<PayMode> PayModes { get; set; } = default!;
+
 		public async Task OnGetAsync()
 		{
-			if (_context.Providers != null)
+			if (_context.PayModes != null)
 			{
-				Providers = await _context.Providers.ToListAsync();
+				PayModes = await _context.PayModes.ToListAsync();
 			}
 		}
-
 	}
 }
--- Pages/Users/Index.cshtml.cs
+++ Pages/Users/Index.cshtml.cs
@@ -1,28 +1,28 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SupermarketWEB.Data;
 using SupermarketWEB.Models;
 
-namespace SupermarketWEB.Pages.Providers
+namespace SupermarketWEB.Pages.Users
 {
-	[Authorize]
-	public class IndexModel : PageModel
+    public class IndexModel : PageModel
     {
 		private readonly SupermarketContext _context;
+
 		public IndexModel(SupermarketContext context)
 		{
 			_context = context;
 		}
-		public IList<Provider> Providers { get; set; } = default!;
+		public IList<User> Users { get; set; } = default!;
+
 		public async Task OnGetAsync()
 		{
-			if (_context.Providers != null)
+			if (_context.Users != null)
 			{
-				Providers = await _context.Providers.ToListAsync();
+				Users = await _context.Users.ToListAsync();
 			}
 		}
-
 	}
 }
+
