19437235ac2fa4da9e0ebbee63be12094c0b8cda
DayanaKCG <165987955+DayanaKCG@users.noreply.github.com>
2024-05-12 21:26:35 -0500

    Add files via upload

--- Data/SupermarketContext.cs
+++ Data/SupermarketContext.cs
@@ -5,12 +5,15 @@ namespace SupermarketWEB.Data
 {
     public class SupermarketContext: DbContext
     {
-        public SupermarketContext(DbContextOptions options) : base(options)
+
+		public SupermarketContext(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
 		public DbSet<Provider> Providers { get; set; }
+        public DbSet<PayMode> PayModes { get; set; }
+        public DbSet<User> Users { get; set; }
 	}
 }
